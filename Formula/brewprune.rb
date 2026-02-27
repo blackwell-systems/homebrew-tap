27c13ca6be997db4e15814e283ae977e1784f1ec
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-02-27 12:04:19 -0700

    chore: bump brewprune to v0.1.5

--- Formula/brewprune.rb
+++ Formula/brewprune.rb
@@ -1,26 +1,26 @@
 class Brewprune < Formula
     desc "Smart Homebrew package cleanup with usage tracking and heuristic-scored removal"
     homepage "https://github.com/blackwell-systems/brewprune"
-    version "0.1.4"
+    version "0.1.5"
     license "MIT"
 
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_darwin_arm64.tar.gz"
-        sha256 "f52b4b78b04ab79e8146279f9289708b81e876180ac7873d8d53ed51cfa88bb4"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.5/brewprune_0.1.5_darwin_arm64.tar.gz"
+        sha256 "0725165b7b36c161c148dc243ca6cb03b8c281534362535939e5cbd84e00a5c1"
       else
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_darwin_amd64.tar.gz"
-        sha256 "9fd319483dd53b193aa11f88bd80e2a59ace4a302fc2748dcec9d2ed8c06149d"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.5/brewprune_0.1.5_darwin_amd64.tar.gz"
+        sha256 "659034efefae09c00fb5172ce034908180ce0c44446ff2c9a26c3c25e1b2432e"
       end
     end
 
     on_linux do
       if Hardware::CPU.arm?
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_linux_arm64.tar.gz"
-        sha256 "ccbea515fe260372a1d90b57c3e922ffe26952a0f229d71fec62e5b010b03c6c"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.5/brewprune_0.1.5_linux_arm64.tar.gz"
+        sha256 "bee72b1e8f0540918ad90afb9b1e42adaac09303cc0858422976bdc706cb7c10"
       else
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_linux_amd64.tar.gz"
-        sha256 "34f14c06394cf0298d4266254b357a8076991c9cd3330ba322eecd2f544f1d55"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.5/brewprune_0.1.5_linux_amd64.tar.gz"
+        sha256 "8b7089a4318c156d0772a757874feb4c86d6eceb621d196e50ff712c46eb676b"
       end
     end
 
