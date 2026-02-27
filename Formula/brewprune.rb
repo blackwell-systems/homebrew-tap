40297206c750d12fd71a8c1263c2fa78b7f3b697
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-02-27 00:44:17 -0700

    chore: bump brewprune to v0.1.4
    Bundles brewprune-shim binary; fixes shim setup when installed via Homebrew.

--- Formula/brewprune.rb
+++ Formula/brewprune.rb
@@ -1,31 +1,32 @@
 class Brewprune < Formula
     desc "Smart Homebrew package cleanup with usage tracking and heuristic-scored removal"
     homepage "https://github.com/blackwell-systems/brewprune"
-    version "0.1.3"
+    version "0.1.4"
     license "MIT"
 
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.3/brewprune_0.1.3_darwin_arm64.tar.gz"
-        sha256 "615dbe8134212e3e32a0a585fe1e3e40c56c99211df5cf224940a0458924d12d"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_darwin_arm64.tar.gz"
+        sha256 "f52b4b78b04ab79e8146279f9289708b81e876180ac7873d8d53ed51cfa88bb4"
       else
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.3/brewprune_0.1.3_darwin_amd64.tar.gz"
-        sha256 "22d674e6fab58f2ccfc0283cc389250ffcfecb8ccafc15a72b6cdc9c09014992"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_darwin_amd64.tar.gz"
+        sha256 "9fd319483dd53b193aa11f88bd80e2a59ace4a302fc2748dcec9d2ed8c06149d"
       end
     end
 
     on_linux do
       if Hardware::CPU.arm?
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.3/brewprune_0.1.3_linux_arm64.tar.gz"
-        sha256 "ec75b4a6d36468433d2dc45023765db618322cd2cd0f3f7cbc0f4d9cfacc7633"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_linux_arm64.tar.gz"
+        sha256 "ccbea515fe260372a1d90b57c3e922ffe26952a0f229d71fec62e5b010b03c6c"
       else
-        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.3/brewprune_0.1.3_linux_amd64.tar.gz"
-        sha256 "aa84b8fb9f88a2e9d4c4c04d8083c35fdd6f588efc17399972c2d25132b6a326"
+        url "https://github.com/blackwell-systems/brewprune/releases/download/v0.1.4/brewprune_0.1.4_linux_amd64.tar.gz"
+        sha256 "34f14c06394cf0298d4266254b357a8076991c9cd3330ba322eecd2f544f1d55"
       end
     end
 
     def install
       bin.install "brewprune"
+      bin.install "brewprune-shim"
     end
 
     test do
