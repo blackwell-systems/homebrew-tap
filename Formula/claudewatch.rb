7df4914eeadaf94e500fca6226232c02d3394b88
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-03-03 21:16:24 -0700

    claudewatch 0.8.1

--- Formula/claudewatch.rb
+++ Formula/claudewatch.rb
@@ -1,28 +1,28 @@
 class Claudewatch < Formula
   desc "Observability for AI-assisted development workflows"
   homepage "https://github.com/blackwell-systems/claudewatch"
-  version "0.8.0"
+  version "0.8.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.0/claudewatch_0.8.0_darwin_arm64.tar.gz"
-      sha256 "647b164b515cf9266c89707f52c8183ca2b717f5a60736b9246198f320bedf27"
+      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.1/claudewatch_0.8.1_darwin_arm64.tar.gz"
+      sha256 "f073ac30a213b731ff6794d8e296d3798dd3a667f1936b22b0ccf00a790a1c6d"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.0/claudewatch_0.8.0_darwin_amd64.tar.gz"
-      sha256 "fa26ed0b30ac338a7358f314e2a424c3dbd949c702b9ed73fb347965c6673ff6"
+      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.1/claudewatch_0.8.1_darwin_amd64.tar.gz"
+      sha256 "942b129150790c96f4f731b9a93c3bc9d20b7bcc66c13473e1776bd2e7f19ff4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.0/claudewatch_0.8.0_linux_arm64.tar.gz"
-      sha256 "6374c7299108c6257e7663d9211fc62f9059cffb24aedb2e96d3b39ef1282197"
+      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.1/claudewatch_0.8.1_linux_arm64.tar.gz"
+      sha256 "74777c8c9b8eb660b6f4a903248f87e472ef1689e6351e57c6e02e0fd994f5d2"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.0/claudewatch_0.8.0_linux_amd64.tar.gz"
-      sha256 "20a130610b0f1f07cf284a5b90204a1a568766ed76c14341a829c5ce615a36f1"
+      url "https://github.com/blackwell-systems/claudewatch/releases/download/v0.8.1/claudewatch_0.8.1_linux_amd64.tar.gz"
+      sha256 "52e2967d03721e5ddda267f8f3063ce51af617d09a4505610582cc9a419dce39"
     end
   end
 
